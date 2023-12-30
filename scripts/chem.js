15c5db3d2918ad79a523a0c736bb194085a48f35
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2023-12-31 01:26:52 +0800

    Chem memorization page

--- scripts/chem.js
+++ scripts/chem.js
@@ -1,1 +1,184 @@
-console.log("test");
+var library = [
+    ["Melting point", "The constant temperature when a substance changes from solid state to become liquid at a specific pressure."],
+    ["Freezing point", "The constant temperature when a substance changes from liquid state to become solid at a specific pressure."],
+    ["Proton number", "The number of protons in the nucleus of an atom."],
+    ["Nucleon number", "The total number of protons and neutrons in the nucleus of an atom."],
+    ["Anion", "A negatively-charged ion."],
+    ["Cation", "A positively-charged ion."],
+    ["Valence shell", "The outermost electron occupied shell of an atom."],
+    ["Valence electron", "Electron at the outermost electron occupied shell of an atom."],
+    ["Isotopes", "Atoms of the same element with the same number of protons but different number of neutrons."],
+    ["Natural abundance", "The percentage of isotopes present in a natural sample of element."],
+    ["Relative atomic mass", "The average mass of an atom of the element compared to 1 of the mass of one carbon-12 atom."],
+    ["Relative molecular mass", "The average mass of the molecule compared to 1 of the mass of one carbon-12 atom."],
+    ["Relative formula mass", "The average mass of the ionic substance compared to 1 of the mass of one carbon-12 atom."],
+    ["Avogadro constant", "The number of particles contained in one mole of substance."],
+    ["Molar mass", "The mass of one mole of substance."],
+    ["Molar volume", "The volume occupied by 1 mol of a gas."],
+    ["Chemical formula", "A representation of a chemical substance using alphabets to represent the atoms and subscript numbers to show the number of each type of atoms found in the elementary entities of the substance."],
+    ["Empirical formula", "The chemical formula that shows the simplest ratio of the number of atoms of each element in a compound."],
+    ["Molecular formula", "The chemical formula that shows the actual number of atoms of each element found in a molecule of a compound."],
+    ["Groups", "The vertical columns of elements in the Periodic Table of Elements."],
+    ["Periods", "The horizontal rows of elements in the Periodic Table of Elements."],
+    ["Basic oxide", "The oxide that can only react with acid to form salt and water."],
+    ["Amphoteric oxide", "The oxide that can react with both acid and alkali to form salt and water."],
+    ["Acidic oxide", "The oxide that can only react with alkali to form salt and water."],
+    ["Ionic bond", "The chemical bond that is formed by the transfer of electrons between a metal atom and a non-metal atom."],
+    ["Covalent bond", "The chemical bond that is formed when non-metal atoms share their electrons to achieve a stable duplet or octet electron arrangement."],
+    ["Single covalent bond", "The chemical bond that is formed when two non-metal atoms share a pair of electrons to achieve a stable duplet or octet electron arrangement."],
+    ["Double covalent bond", "The chemical bond that is formed when two non-metal atoms share two pairs of electrons to achieve a stable octet electron arrangement."],
+    ["Triple covalent bond", "The chemical bond that is formed when two non-metal atoms share three pairs of electrons to achieve a stable octet electron arrangement."],
+    ["Hydrogen bond", "An attraction forces between hydrogen atom that has bonded with an atom of high electronegativity, such as nitrogen, oxygen or fluorine with nitrogen, oxygen or fluorine in another molecule."],
+    ["Dative bond (Coordinate bond)", "A type of covalent bond where the electron pair that is shared comes from one atom only."],
+    ["Metallic bond", "The chemical bond that formed from the electrostatic attraction force between the sea of electrons and the positively-charged metal ions."],
+    ["Acid", "The chemical substance that ionises in water to produce hydrogen ions."],
+    ["Basicity of acids", "The number of hydrogen ions that can be produced by an acid molecule that ionises in water."],
+    ["Base", "The substance that reacts with acids to produce salt and water only."],
+    ["Alkali", "Chemical substances that ionise in water to produce hydroxide ions"],
+    ["pH", "A logarithmic measure of the concentration of hydrogen ions in an aqueous solution."],
+    ["Strong acid", "The chemical substance that ionises completely in water to produce a high concentration of hydrogen ions."],
+    ["Weak acid", "The chemical substance that ionises partially in water to produce a low concentration of hydrogen ions"],
+    ["Strong alkali", "The chemical substance that ionises completely in water to produce a high concentration of hydroxide ions."],
+    ["Weak alkali", "The chemical substance that ionises partially in water to produce a low concentration of hydroxide ions."],
+    ["Concentration of a solution", "A measurement that shows the quantity of solute dissolved in a unit volume of solution."],
+    ["Standard solution", "A solution with known concentration."],
+    ["Neutralisation", "A reaction between an acid and an alkali (base) to produce salt and water only."],
+    ["Titration method", "A quantitative analysis method to determine the volume of acid needed to completely neutralise a given volume of alkali and vice versa."],
+    ["End point", "The point in the titration at which the acid-base indicator changes colour."],
+    ["Salt", "An ionic compound formed when the hydrogen ion from the acid is replaced with the metal ion or the ammonium ion."],
+    ["Soluble salt", "A salt that dissolves in water at room temperature"],
+    ["Non-soluble salt (Insoluble salt)", "A salt that does not dissolve in water at room temperature."],
+    ["Continuous variation method", "A method used to construct the ionic equation for the formation of insoluble salts."],
+    ["Qualitative analysis of a salt", "A technique used to identify the cation and anion present in a salt by analysing its physical and chemical properties."],
+    ["Rate of reaction", "The changes in the quantity of the reactant per unit time or the changes in the quantity of product per unit time. The decrease in the quantity of the reactant per unit time. The increase in the quantity of product per unit time."],
+    ["Average rate of reaction", "The average value for the rate of reaction that occurs in a particular time interval."],
+    ["Instantaneous rate of reaction", "The rate of reaction at a particular point of time."],
+    ["Catalysts", "Chemical substances that alter the rate of chemical reactions without undergoing any chemical changes at the end of the reaction."],
+    ["Effective collision", "The collision of reactant particles that have energy equal to or more than the activation energy and in the correct orientation."],
+    ["Activation energy", "An energy barrier that needs to be overcomed before a reaction can take place."],
+    ["Exothermic reactions", "Reactions that release heat energy to the surroundings."],
+    ["Endothermic reactions", "Reactions that absorb heat energy from the surroundings are called"],
+    ["Alloy", "A mixture of two or more elements where the main element is a metal."],
+    ["Composite material", "A material made from combining two or more non-homogeneous substances, that is matrix substance and strengthening substance."],
+    ["Ceramic", "A solid made up of inorganic and non-metallic substances"],
+
+    ["Redox reaction", "A chemical reaction where oxidation and reduction occur simultaneously."],
+    ["Oxidation reaction", "A chemical reaction takes place when a reactant gains oxygen // A chemical reaction takes place when a reactant loses hydrogen // A chemical reaction takes place when a reactant loses electron // A chemical reaction takes place when the oxidation number of an element increases *** Depend on the condition"],
+    ["Reduction reaction", "A chemical reaction takes place when a reactant loses oxygen // A chemical reaction takes place when a reactant gains hydrogen // A chemical reaction takes place when a reactant gains electron // A chemical reaction takes place when the oxidation number of an element decreases *** Depend on the condition"],
+    ["Oxidation number / Oxidation state", "The charge of the elements in a compound if the transfer of electrons occurs in an atom to form chemical bonds with other atoms."],
+    ["Electropositivity", "The tendency of atoms to release electrons to form cations."],
+    ["Electrochemical series", "A series of metals that are arranged according to the standard electrode potential, E0 in the order of most negative to most positive."],
+    ["Electrode potential", "The potential difference produced when an equilibrium is established between metal M and the aqueous solution containing metal Mn+ ions in a half-cell."],
+    ["Simple chemical cell / Voltaic cell / Galvanic cell", "A chemical cell produced when two metal plates are dipped into an electrolyte and connected with connecting wires."],
+    ["Negative terminal (Anode in a voltaic cell)", "The metal plate in a voltaic cell which has the more negative E0 value."],
+    ["Positive terminal (Cathode in a voltaic cell)", "The metal plate in a voltaic cell which has the more positive E0 value."],
+    ["Electrolytes", "Substances that can conduct electricity in either the molten state or aqueous solution and undergo chemical changes."],
+    ["Non-electrolytes", "Substances that cannot conduct electricity in all states."],
+    ["Conductor", "Substances that conduct electricity in solid or molten state, but do not undergo chemical changes."],
+    ["Electrolysis", "A process whereby compounds in the molten state or an aqueous solution decompose into their constituent elements by passing electricity through them."],
+    ["Anode", "Electrode that connects to the positive terminal of the battery."],
+    ["Cathode", "Electrode that connects to the negative terminal of the battery."],
+    ["Anion", "Negatively charged ion"],
+    ["Cation", "Positively charged ion"],
+    ["Corrosion of metal", "A redox reaction where the metal is oxidised spontaneously when the metal atoms release electrons to form metal ions."],
+    ["Rusting of iron", "A chemical reaction that occurs when iron is exposed to oxygen and water, and undergoes a redox reaction."],
+    ["Carbon compounds", "Compounds that contain carbon as their constituent element."],
+    ["Hydrocarbon", "Organic compounds containing only hydrogen and carbon."],
+    ["Non hydrocarbon", "Organic compounds containing carbon and hydrogen and other elements, such as oxygen, nitrogen, phosphorus or halogens."],
+    ["Saturated hydrocarbon", "Hydrocarbons containing only single bonds between carbon atoms."],
+    ["Unsaturated hydrocarbon", "Hydrocarbons containing at least one double bond or triple bond between carbon atoms."],
+    ["Fractional distillation ", "The process to separate the fractions of hydrocarbons in petroleum at different temperatures according to the size of the hydrocarbons."],
+    ["Cracking", "The process of breaking long chain hydrocarbons into smaller hydrocarbons."],
+    ["Homologous series", "Groups of compounds with the same general formula, functional group and chemical properties."],
+    ["Substitution reaction", "A chemical reaction occurs when each hydrogen atom, H in an alkane molecule is substituted one by one with halogen atoms, until all the hydrogen atoms, H have been substituted."],
+    ["Addition reaction", "A chemical reaction occurs when another atom is added to each carbon atom, C at the double bond C=C to form a single covalent bond C-C."],
+    ["Molecular formula", "A chemical formula that shows the type and actual number of atoms of each element in a molecule."],
+    ["Structural formula", "A chemical formula that shows the type of bond and how the atoms in a molecule are bonded to each other."],
+    ["Fermentation", "The process in which yeast acts on carbohydrates (sugar or starch) to produce ethanol and carbon dioxide in the absence of oxygen."],
+    ["Structural isomerism", "A phenomenon where a compound has the same molecular formula but with two or more different structural formulae."],
+    ["Isomers", "Molecules that have the same molecular formula but different structural formulae."],
+    ["Chain isomerism", "A phenomenon where the isomers have different arrangements of carbon chains; either straight chain or branched chain."],
+    ["Position isomerism", "A phenomenon where the isomers have different positions of functional group on the same carbon chain."],
+    ["Exothermic reactions", "Chemical reactions that release heat to the surroundings."],
+    ["Endothermic reactions", "Chemical reactions that absorb heat from the surroundings."],
+    ["Heat of reaction", "Heat change of one mole of reactant that reacts or one mole of product that is formed."],
+    ["Heat of precipitation", "Heat change when one mole of precipitate is formed from their ions in an aqueous solution."],
+    ["Heat of displacement", "Heat change when one mole of a metal is displaced from its salt solution by a more electropositive metal."],
+    ["Heat of neutralisation", "Heat change when one mole of water is formed from the reaction between an acid and an alkali."],
+    ["Heat of combustion", "Heat released when one mole of a substance is completely burnt in excess oxygen."],
+    ["Fuel value", "The amount of heat energy released when one gram of fuel is completely burnt in excess oxygen."],
+    ["Polymer", "A long chain molecule that is made from a combination of many repeating basic units."],
+    ["Thermoplastic polymers", "Polymers that can be repeatedly remoulded upon heating and can be recycled."],
+    ["Thermosetting polymers", "Polymers that cannot be remoulded after heating."],
+    ["Elastomer polymers", "Polymers that can be stretched and can return to their original shape when released."],
+    ["Addition polymerisation", "The polymerisation reaction that occurs when monomers that have double covalent bonds between two carbon atoms, C=C react with one another."],
+    ["Condensation polymerisation", "The polymerisation reaction involves at least two different monomers consist of two functional groups to produce a polymer and another by-product such as water molecule or hydrogen chloride molecule."],
+    ["Latex", "A white fluid obtained from rubber trees when the tree bark is tapped."],
+    ["Vulcanisation", "A process of producing rubber that is more elastic and with better quality through the production of cross-links between polymer chains."],
+    ["Synthetic rubber", "A synthetic polymer that is elastic in nature or elastomer polymer."],
+    ["Oils / fats", "Esters produced through the reaction between fatty acids and glycerol (propan-1,2,3-triol)."],
+    ["Saturated fats", "Esters produced through the reaction between saturated fatty acids and glycerol (propan-1,2,3-triol)."],
+    ["Unsaturated fats", "Esters produced through the reaction between unsaturated fatty acids and glycerol (propan-1,2,3-triol)."],
+    ["Soaps", "Sodium or potassium fatty acid salts."],
+    ["Detergents", "Sodium salts of sulphonic acids."],
+    ["Saponification", "The process of hydrolysis of oils or fats by alkalis to produce a soap."],
+    ["Hydrophilic part", "The structure of soap anion and detergent anion that is soluble in water."],
+    ["Hydrophobic part", "The structure of soap anion and detergent anion that is soluble in oil or grease."],
+    ["Hard water", "Water containing calcium ions, Ca2+ and magnesium ions, Mg2+."],
+    ["Scum", "The insoluble salt formed when soap anions combine with the calcium ions, Ca2+ and magnesium ions, Mg2+ in the hard water."],
+    ["Food additives", "Natural or synthetic ingredients added to food to prevent damage or to improve the appearance, taste or texture."],
+    ["Medicines", "Chemicals used to help with the treatment or prevention of diseases."],
+    ["Traditional medicines", "The medicines that obtained from herbal plants or animals and are not chemically processed."],
+    ["Cosmetics", "Materials or products that are used externally to cleanse, protect or enhance one's appearances."],
+    ["Nanoscience", "A study on processing of substances at nanoscale that are between 1 nanometre to 100 nanometres."],
+    ["Nanotechnology", "A development of substances or gadgets using the properties of nanoparticles."],
+    ["Green technology", "A technology or application developed to minimise the negative effects of harmful human activities."],
+    ["Wastewater", "A liquid waste that consists of human waste, food waste, oil and chemicals"],
+]
+
+var title = document.getElementById("title");
+var answer = document.getElementById("answer");
+var userInput = document.getElementById("prompt");
+var button = document.getElementById("progress");
+
+var credits = document.getElementById("credits");
+
+userInput.addEventListener('keyup', (key) => {
+    if (key.code == "Enter") {
+        progress()
+    }
+})
+
+function generate() {
+    while (true) {
+        let target = library[Math.round(Math.random() * (library.length - 1))];
+        if (target[0] == title.innerHTML) {
+            continue;
+        }
+
+        title.innerHTML = target[0];
+        answer.innerHTML = target[1];
+        updatePrompt(false);
+        break;
+    }
+}
+
+function updatePrompt(s) {
+    answer.ariaLabel = s ? "open" : "closed";
+    button.innerHTML = s ? "next" : "check";
+}
+
+function progress() {
+    if (button.innerHTML == "next") {
+        generate();
+        userInput.value = "";
+    } else {
+        updatePrompt(true);
+    }
+}
+
+generate();
+
+function revealCredits() {
+    credits.ariaLabel = credits.ariaLabel == "open" ? "closed" : "open";
+}
