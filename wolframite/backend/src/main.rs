e45c16fcd928da86f23361ff542e47a8b7182633
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2024-03-01 00:05:57 +0800

    URL rewriting testing

--- wolframite/backend/src/main.rs
+++ wolframite/backend/src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
     println!("Hello, world!");
+
+    // wolframite -> wolframite/index.html
+    // wolframite/login -> wolframite/login.html
+    // wolframite/signup -> wolframite/signup.html
 }
 
