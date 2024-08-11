f5b5887d40c4d3301ad502f0b0016e0d4fe980c4
ajian_nedo <asianerd28@gmail.com>
2024-08-12 00:45:21 +0800

    Updated backend paths

--- source.js
+++ source.js
@@ -1,14 +1,16 @@
 // const BACKEND_ADDRESS = 'http://www.ozmium.xyz';
 // const BACKEND_ADDRESS = 'https://3.106.177.190:8000';
 
-const AURUM_BACKEND_ADDRESS = 'http://127.0.0.1:8000';
-// const BACKEND_ADDRESS = 'https://ozmium.xyz/backend';
+// const AURUM_BACKEND_ADDRESS = 'http://127.0.0.1:8000';
+const AURUM_BACKEND_ADDRESS = 'https://ozmium.xyz/backend';
 
-const CHRONOS_BACKEND_ADDRESS = 'http://127.0.0.1:8001';
-const ATHENA_BACKEND_ADDRESS = 'http://127.0.0.1:8002';
+// const CHRONOS_BACKEND_ADDRESS = 'http://127.0.0.1:8001';
+// const ATHENA_BACKEND_ADDRESS = 'http://127.0.0.1:8002';
+const CHRONOS_BACKEND_ADDRESS = 'http://ozmium.xyz/chronos_backend';
+const ATHENA_BACKEND_ADDRESS = 'http://ozmium.xyz/athena_backend';
 
-const SOTERIUS = 'http://127.0.0.1:8100';
-// const SOTERIUS = 'https://ozmium.xyz/soterius_backend';
+// const SOTERIUS = 'http://127.0.0.1:8100';
+const SOTERIUS = 'https://ozmium.xyz/soterius_backend';
 // uri rewriting in place
 
 async function sendGetRequest(url, func) {
