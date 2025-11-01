00ab1a913c90d611ff9190623902f0751a3175fe
ajian_nedo <asianerd28@gmail.com>
2025-11-01 23:34:19 +0800

    (WIP) Query functionality

---
+++ homepage.js
@@ -0,0 +1,62 @@
+const selectedTagContainer = document.querySelector("#tags:first-of-type");
+const tagContainer = document.querySelector("#tags:last-of-type");
+
+var tags = [
+    'web3',
+    'fullstack',
+    'c#',
+    'c++',
+    'rust',
+    'python',
+    'solidity',
+    'react',
+    'html/css',
+    'firestore',
+    'vercel',
+]
+
+var selectedTags = [];
+
+function updateContainers() {
+    selectedTagContainer.innerHTML = '';
+    tagContainer.innerHTML = '';
+
+    let selectedTagContainerNew = '';
+    let tagContainerNew = '';
+
+    let unselected = tags.filter(x => !selectedTags.includes(x));
+    unselected.forEach((e) => {
+        tagContainerNew += `<div class="tag" onclick="select('${e}')">
+            <h2>${e.toUpperCase()}</h2>
+            <div>
+                <img src="./assets/cross.png">
+            </div>
+        </div>`;
+    });
+
+    selectedTags.forEach((e) => {
+        selectedTagContainerNew += `<div class="tag" selected onclick="deselect('${e}')">
+            <h2>${e.toUpperCase()}</h2>
+            <div>
+                <img src="./assets/cross.png">
+            </div>
+        </div>`;
+    });
+
+    tagContainer.innerHTML = tagContainerNew;
+    selectedTagContainer.innerHTML = selectedTagContainerNew;
+}
+
+function deselect(e) {
+    selectedTags = selectedTags.filter(i => i !== e);
+
+    updateContainers();
+}
+
+function select(e) {
+    selectedTags.push(e);
+
+    updateContainers();
+}
+
+updateContainers();
